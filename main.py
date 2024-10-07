be36e211b8ccb00a3e8e341f38878a16a4751795
polinka <pgrisko5@gmail.com>
2024-10-07 15:17:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-float_list = [1.5,2.3,3.7,4.8,5.2]
-integer_parts =[int(x)for x in float_list]
-print(integer_parts)
+float_list = [1.5,2.3,3.7,4.8,5.2] #список чисел
+integer_parts =[int(x)for x in float_list] #формула расчета целых частей
+print(integer_parts) #вывод целых частей на экран
